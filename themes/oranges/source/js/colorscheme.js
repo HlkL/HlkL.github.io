3d414e9b26fd2b78e03076f8fec6e69e341a547e
HG <hglkl6854@gmail.com>
2024-11-06 00:17:11 +0800

    评论颜色更改

--- themes/oranges/source/js/colorscheme.js
+++ themes/oranges/source/js/colorscheme.js
@@ -3,6 +3,21 @@ let switchHandle = document.querySelector('#switch-color-scheme')
 let themeIcon = document.querySelector('#theme-icon')
 var html = document.documentElement
 
+const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+systemPrefersDark.addEventListener('change', (e) => {
+    let colorMode = 'light'
+    if(e.matches) {
+        html.setAttribute('color-mode', 'dark')
+        themeIcon.classList = 'iconfont icon-sun'
+        colorMode = 'dark'
+    } else {
+        html.setAttribute('color-mode', 'light')
+        themeIcon.classList = 'iconfont icon-moon'
+        colorMode = 'light'
+    }
+    localStorage.setItem('color-mode', colorMode)
+})
+
 const switchMode = () => {
     let attr = html.getAttribute('color-mode')
     let colorMode = 'light'
